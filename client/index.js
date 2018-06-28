814d7517e85ee48d7110f13e3091199b4e43a7c6
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP-2.home>
2018-06-27 20:05:00 -0400

    Added fullpage and mustard ui

--- client/index.js
+++ client/index.js
@@ -1,7 +1,25 @@
-// Import from the module './whiteboard':
-//   The default export, naming it draw,
-//   An export named `events`, calling it `whiteboard`.
-import whiteboard, {draw} from './whiteboard'
+// $.fn.fullpage
+// $(document).ready(() => {
+// })
+let level = 0;
+let slide = 0;
 
-// Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true)
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1aac9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        controlArrows: true,
+        menu: '#menu',
+        scrollingSpeed: 1000,
+        loopHorizontal: false,
+        onLeave: (origin, destination)=>{
+            console.log('LEVEL MOVING FROM', origin, 'TO', destination)
+            level = destination
+        },
+        onSlideLeave: (section, origin, destination)=>{
+            console.log('SLIDER MOVING FROM', origin, 'TO', destination)
+            slide = destination;
+            console.log('s', slide, 'l', level)
+        }
+    });
+})

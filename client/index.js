980e11e3cfb8d19a8556f0a5029386254cb97dfd
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro-2.local>
2018-06-28 10:41:01 -0400

    navbar underline detail tracks section position

--- client/index.js
+++ client/index.js
@@ -7,11 +7,18 @@ let slide = 0;
 $(document).ready(function() {
     let menuItems = document.getElementsByClassName('menuItem');
     const clearBorder = () => Array.prototype.forEach.call(menuItems, (item) => {
-        console.log('clearing!')
         item.classList.remove('menuItemBorder')
-        console.log(item);
     })
-    // menuItems[0].dispatchEvent(new Event('click'))
+
+    Array.prototype.forEach.call(menuItems, (item => {
+        item.addEventListener('click', (ev) => {
+            clearBorder();
+            if(!item.classList.contains('menuItemBorder')) item.classList.add('menuItemBorder');
+        })
+    }))
+
+    menuItems[0].dispatchEvent(new Event('click'))
+
     $('#fullpage').fullpage({
         sectionsColor: ['#1aac9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
@@ -23,22 +30,18 @@ $(document).ready(function() {
             console.log('LEVEL MOVING FROM', origin, 'TO', destination)
             level = destination
             clearBorder()
-            if(!menuItems[level - 1].classList.contains('menuItemBorder')) menuItems[level - 1].dispatchEvent(new Event('click'))
+            if(!menuItems[level - 1].classList.contains('menuItemBorder')) {
+                console.log('moving! adding to class')
+                menuItems[level - 1].dispatchEvent(new Event('click'))
+            }
         },
         onSlideLeave: (section, origin, destination)=>{
             console.log('SLIDER MOVING FROM', origin, 'TO', destination)
-            let items = Array.prototype.slice.call(menuItems, 0);
+            clearBorder()
             slide = destination;
             console.log('s', slide, 'l', level)
             console.log(items)
         }
     });
-
-    Array.prototype.forEach.call(menuItems, (item => {
-        item.addEventListener('click', (ev) => {
-            clearBorder();
-            if(!ev.target.classList.contains('menuItemBorder')) ev.target.classList.add('menuItemBorder');
-        })
-    }))
 })
 
